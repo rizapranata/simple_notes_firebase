140ad6f098f3b03802eb4b530e535d0227989c71
Riza Pranata <riza.ari@dkatalis.com>
2020-07-28 11:41:56 +0700

    instalasi dan integrasi firebase

---
+++ src/config/firebase/index.js
@@ -0,0 +1,18 @@
+ import firebase from 'firebase/app';
+ 
+ // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBzCqMZk54jUXI-ALdfoopml4R848dFyxc",
+    authDomain: "simple-notes-firebase-1a86d.firebaseapp.com",
+    databaseURL: "https://simple-notes-firebase-1a86d.firebaseio.com",
+    projectId: "simple-notes-firebase-1a86d",
+    storageBucket: "simple-notes-firebase-1a86d.appspot.com",
+    messagingSenderId: "647866029049",
+    appId: "1:647866029049:web:f4e16d4bdbe58e7fe9e328",
+    measurementId: "G-2MVFR1HPS5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+
+  export default firebase;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/pages/App';
 import * as serviceWorker from './serviceWorker';
+import firebase from './config/firebase';
+
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
